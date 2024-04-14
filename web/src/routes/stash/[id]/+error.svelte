49f1245bb79919fd5bf1b246f9828e1f5b95c642
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2024-04-14 19:29:10 +0530

    feat(frontend): add error pages

---
+++ web/src/routes/stash/[id]/+error.svelte
@@ -0,0 +1,11 @@
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Not Allowed</h1>
+      <p class="py-6">
+        This stash either doesnot exists or you don't have the necessary
+        permissions to access it.
+      </p>
+    </div>
+  </div>
+</div>
